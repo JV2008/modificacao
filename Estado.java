91618350f430b95f0ec2ea6e4631d83af447b9e2
JV2008 <joaovictor25032@gmail.com>
2024-09-25 16:46:11 -0300

    primeiro commit

---
+++ Estado.java
@@ -0,0 +1,26 @@
+package classdefault;
+
+abstract class Estado {
+	protected String nome;
+	protected String sigla;
+	
+	public Estado(String nome,String sigla) {
+		
+		this.nome= nome;
+		this.sigla= sigla;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+}
